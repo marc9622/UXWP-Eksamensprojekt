7248b5f2faa7a877403fe569cb817d9e1bfbd52b
mohdh <mkdhikil@gmail.com>
2023-05-27 17:25:25 +0200

    backend added institutions

---
+++ backend/Institution/Institutions.model.js
@@ -0,0 +1,19 @@
+import * as fs from "fs/promises";
+const UNI_FILE = "./Institution/Institutions.json";
+
+export async function getAll() {
+    try {
+        let institutionText = await fs.readFile(UNI_FILE, 'utf8');
+        let uniList = JSON.parse(institutionText);
+        console.log(uniList);
+        return uniList;
+    } catch (err) {
+      if (err.code === "ENOENT") {
+        // file does not exits
+        await save([]); // create a new file with empty array
+        console.log("No unis found, returning empty array");
+        return []; // return empty array
+      } // // cannot handle this exception, so rethrow
+      else throw err;
+    }
+  }
--- backend/index.js
+++ backend/index.js
@@ -2,44 +2,43 @@ import express from "express";
 import cors from "cors";
 import fileUpload from "express-fileupload";
 import { createRandomID } from "./utility.js";
-import * as books from "./books/books.model.js";
 import * as users from "./users/users.model.js"
 import * as rooms from "./rooms/rooms.model.js"
 import * as bookings from './bookings/bookings.model.js'
+import * as unis from "./Institution/Institutions.model.js"
 
 const app = express();
 const PORT = 3001; // fetch url: localhost:3001
+const router = express.Router();
+
 
 app.use(express.json());
 app.use(cors());
 app.use(fileUpload());
 app.use(express.static("public"));
 app.use("/images", express.static("images"));
+app.use(router);
 
-app.post("/upload", (request, response) => {
-  // Get the file that was set to our field named "image"
-  console.log(request.files);
-  const { file } = request.files;
 
-  // If no image submitted, exit
-  if (!file) return response.sendStatus(400);
-
-  // Move the uploaded image to our upload folder
-  const fileName = createRandomID("book") + file.name;
-  file.mv("./images/" + fileName);
-  response.send(
-    JSON.stringify({
-      status: 200,
-      body: { imagePath: `http://localhost:${PORT}/images/${fileName}` },
-    })
-  );
+router.get('/unilist', async (request, response) => {
+  try {
+    var uniList = await unis.getAll();
+    response.setHeader('Content-Type', 'application/json');
+    console.log(uniList);
+    response.json(uniList);
+  } catch (err) {
+    // Handle any errors
+    console.error(err);
+    response.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
-// vJERES KODE HERv
+
+router.get('/', (request, response) => {
+  response.sendFile('C:/Users/mohdh/IdeaProjects/UXWP-Eksamensprojekt/backend/side.html');
+});
 
 
-
-// ^JERES KODE HER^
 
 app.listen(PORT, function (err) {
   if (err) console.log("Error in server setup");
